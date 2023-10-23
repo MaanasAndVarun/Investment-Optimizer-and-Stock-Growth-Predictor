cec9d323df520d7805a4a53dccf44e5c13d5f4c9
MaanasAndVarun <86464642+MaanasAndVarun@users.noreply.github.com>
2023-10-23 11:22:03 +0530

    Update Stonks.py

--- Stonks.py
+++ Stonks.py
@@ -612,7 +612,6 @@ if a=='Understand':
         model_fit = model.fit()
         output = model_fit.forecast()
         yhat = output[0]
-        st.write(yhat)
         model_predictions.append(yhat)
         true_test_value = test_data[time_point]
         history.append(true_test_value)
